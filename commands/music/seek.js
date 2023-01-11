1730d0978e051ab64b64d5b55846010af0f9ea55
Oncl F2roq <97564562+6fq@users.noreply.github.com>
2023-01-11 17:16:55 +0200

    Update seek.js

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -20,7 +20,7 @@ module.exports = {
 
         const timeToMS = ms(inter.options.getString('time'));
 
-        if (timeToMS >= queue.current.durationMS) return inter.reply({ content:`> **The indicated time is higher than the total time of the current song ${inter.member}... try again ? <:2342:1051520390339502222> \n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**..* **`, ephemeral: true });
+        if (timeToMS >= queue.current.durationMS) return inter.reply({ content:`> **The indicated time is higher than the total time of the current song ${inter.member}... try again ? \n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**..* **`, ephemeral: true });
 
         await queue.seek(timeToMS);
 
